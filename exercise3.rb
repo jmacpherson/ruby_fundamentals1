658c079a8350286d9951a8062f88cafd45d2d3c4
jmacpherson <jd.macpherson@gmail.com>
2013-10-22 11:22:33 -0400

    exercise3.rb, took age, returned year born (roughly)

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,9 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi, #{name}!"
+
+puts "How old are you, #{name}?"
+age = gets.chomp.to_i
+
+year = 2013
+puts "Since you are #{age}, you must have been born in #{year - age}"
