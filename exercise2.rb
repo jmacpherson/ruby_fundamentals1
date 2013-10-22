cb0bc56df070af5c0f9a304342fe75dc8b3384e4
jmacpherson <jd.macpherson@gmail.com>
2013-10-22 11:05:15 -0400

    wrote exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#Calculate tip
+puts "A 15% tip on a $55 meal should be #{55*0.15}."
+
+#Adding string and integer
+puts "Seventy three = " + 73.to_s
+
+#String interpolation example
+puts "45628 multiplied by 7839 is #{45628 * 7839}"
+
+#Boolean expressions (should equal true)
+puts (true && false) || (false && true) || !(false && false)
